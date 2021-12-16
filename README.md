cee4734dcd296bb77f126fd20f42ef06d80e8ab7
rmalik25 <50649280+rmalik25@users.noreply.github.com>
2021-12-16 11:43:18 +1100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starlight Travel by LaunchDarkly
+
+![Toggle](https://cdn.glitch.me/d9b387c2-1a95-4f82-b12f-3083a1599166%2FToggle-thumbs-up.png?v=1638541420697)
+
+Welcome to Starlight Travel! Here you will learn how to how to dark launch like a pro! Use this app as your guide and sandbox for learning the basics of LaunchDarkly.
---
+++ server.py
@@ -0,0 +1,44 @@
+# TODO: Add the LaunchDarkly SDK to this so we can flag new features
+import os
+from flask import Flask, render_template
+import ldclient
+from ldclient.config import Config
+import json
+
+
+
+# Paste your SDK key inside the `.env` file. Don't paste it
+# here or you'll expose your SDK key to anyone who opens
+# this app. Instead, use the environment variable to
+ldclient.set_config(Config("sdk-4f0ff5b2-9c06-463f-9ac7-ab24588b1a84"))
+client = ldclient.get()
+
+
+
+
+app = Flask(__name__, static_folder='public', template_folder='views')
+
+    
+@app.route('/')
+def pricing():
+    """Displays the pricing page."""
+
+    # Since this page doesn't require a logged in user,
+    # tell LaunchDarkly that this is an anonymous user.
+    f = open('user.json')
+    data = json.load(f)
+
+    user = {
+      "key": data['key'],
+      "anonymous": False,
+      "country": data['country'],
+      "firstName": data['firstName']
+    }
+    is_tier_3_enabled = client.variation('firstflag', user, False)
+    print("Flag: " + str(is_tier_3_enabled))
+    return render_template('pricing.html', is_tier_3_enabled=is_tier_3_enabled)
+
+
+  
+if __name__ == '__main__':
+    app.run()
